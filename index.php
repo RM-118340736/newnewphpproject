5d861c3f5482a1029f6cd3c88bb4972cdb3f17d0
118340736 <118340736@BISB58VL.bis.ad.ucc.ie>
2019-03-07 16:19:14 +0000

    -a -m: "some new message"

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ and open the template in the editor.
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <style>
+    
+    
   
     h1 {
         color:#000900;     
@@ -19,6 +21,7 @@ and open the template in the editor.
         background-image: url(background.jpg)       
     }
  h2 {
+     
       color:white;
       font-size:40px;
       text-align:center;  
